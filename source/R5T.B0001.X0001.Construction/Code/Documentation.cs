444b3b5c12b59cc73cc9d69afb2e66db37263160
davidcoats <david@getsafetycone.com>
2022-02-06 21:34:41 -0800

    Initial commit of source-stub.

---
+++ source/R5T.B0001.X0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0001.X0001.Construction
+{
+    /// <summary>
+    /// Construction project for Rivet-specific extensions to TypeName and related bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.B0001.X0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0001.X0001
+{
+    /// <summary>
+    /// Rivet-specific extensions to TypeName and related bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
